1ff657a586971e0053f353c809c36217087c74af
Ben Actis <grumpycat@fb.com>
2020-12-29 12:53:33 -0800

    Fix open quotes

--- Casks/dbxcli.rb
+++ Casks/dbxcli.rb
@@ -3,7 +3,7 @@ cask "dbxcli" do
   sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
 
   url "https://download.sweetscape.com/010EditorMac64Installer#{version}.dmg"
-  url "https://github.com/dropbox/dbxcli/releases/download/#{version}/dbxcli-darwin-amd64
+  url "https://github.com/dropbox/dbxcli/releases/download/#{version}/dbxcli-darwin-amd64"
   appcast "https://github.com/dropbox/dbxcli/"
   name "010 Editor"
   homepage "https://www.dropbox.com""
